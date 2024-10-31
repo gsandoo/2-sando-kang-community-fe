b132a1bbc2f3e398cfc314bf96cf0a191c693151
sakang <rogersddt@naver.com>
2024-10-31 19:23:35 +0900

    Fix: 게시글 작성 뒤로가기 설정

--- js/make post.js
+++ js/make post.js
@@ -1,9 +1,10 @@
 // TODO: 요구사항에 맞춘 추가 작업 필요
 // 게시글 작성 버튼 클릭 시 게시글 작성 페이지로 이동
-const modifyBtn = document.querySelector(".back");
+const backBtn = document.querySelector(".page-back");
 const title = document.getElementById('title');
 const content = document.getElementById('content');
 const submit = document.getElementById('submit-button');
+const fileInput = document.getElementById('image');
 
 
 function validateForm() {
@@ -29,6 +30,22 @@ function validateForm() {
 
 title.addEventListener('input', validateForm)
 content.addEventListener('input', validateForm);
+
+backBtn.addEventListener('click', () => {
+    history.go(-1);
+})
+
+fileInput.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    
+    if (file) {
+        const updatedImg = URL.createObjectURL(file);
+        const fileName = updatedImg.split('/').pop();
+        document.querySelector('.file-name').innerText = fileName;
+    }else {
+        console.log("파일이 선택되지 않았습니다.");
+      }
+});
 
 function handleLocation(url) {
     window.location.href = url
